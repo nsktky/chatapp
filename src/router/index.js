c1d93c901d881c30a33c88b4cc03c8d5775b87af
nsktky <nosaka.takuya11@gmail.com>
2022-09-29 18:53:04 +0900

    ユーザー一覧作成

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,17 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+// import HomeView from '../views/HomeView.vue'
+import UserList from '../views/UserList.vue'
+import ChatBoard from '../views/ChatBoard.vue'
+
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView
+    name: 'UserListme',
+    component: UserList
   },
   {
     path: '/about',
@@ -17,7 +20,12 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+  },
+  {
+    path: '/chat',
+    name: 'ChatBoard',
+    component: ChatBoard
+  },
 ]
 
 const router = new VueRouter({
